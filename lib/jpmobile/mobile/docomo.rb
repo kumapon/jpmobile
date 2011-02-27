8cce26610c2b8e234f06fb5e9d396163d4b90796
Rikiya Endo <r-endo@smalltalk.co.jp>
2011-02-27 21:52:23 +0900

    リクエスト時に絵文字のエンコードをコメントアウト

--- lib/jpmobile/mobile/docomo.rb
+++ lib/jpmobile/mobile/docomo.rb
@@ -73,7 +73,8 @@ module Jpmobile::Mobile
       # 文字コードを UTF-8 に変換
       str = Jpmobile::Util.sjis_to_utf8(str)
       # 絵文字を数値参照に変換
-      str = Jpmobile::Emoticon.external_to_unicodecr_docomo(Jpmobile::Util.sjis(str))
+      # なんか邪魔する・・・
+      #str = Jpmobile::Emoticon.external_to_unicodecr_docomo(Jpmobile::Util.sjis(str))
       # 数値参照を UTF-8 に変換
       Jpmobile::Emoticon.unicodecr_to_utf8(str)
     end
