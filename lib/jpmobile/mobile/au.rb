bfd62032cdc2f189fbf34e3cf9d48bc32253f18c
runamoon <runa.hatuki@gmail.com>
2011-03-08 18:18:01 -0800

    auの文字コード変換をもとにもどす

--- lib/jpmobile/mobile/au.rb
+++ lib/jpmobile/mobile/au.rb
@@ -80,11 +80,10 @@ module Jpmobile::Mobile
 
     # 文字コード変換
     def to_internal(str)
+      # 絵文字を数値参照に変換
+      str = Jpmobile::Emoticon.external_to_unicodecr_au(Jpmobile::Util.sjis(str))
       # 文字コードを UTF-8 に変換
       str = Jpmobile::Util.sjis_to_utf8(str)
-      # 絵文字を数値参照に変換
-      # なんか邪魔する・・・
-      #str = Jpmobile::Emoticon.external_to_unicodecr_au(Jpmobile::Util.sjis(str))
       # 数値参照を UTF-8 に変換
       Jpmobile::Emoticon::unicodecr_to_utf8(str)
       # 半角->全角変換
